e20c2ffe25a54939635256fc0822f9c45b11dcd5
arektar <zander5@mail.ru>
2016-12-30 20:18:12 +0300

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -1,20 +1,17 @@
 import os, filecmp
 
 def are_files_duplicates(file_path1, file_path_2):
-    if os.path.basename(file_path1)==os.path.basename(file_path_2):
         if filecmp.cmp(file_path1,file_path_2):
             return True
     return False
 
 
 if __name__ == '__main__':
-    root=input("Write watching dir way: ")
-    root=root+"\\"
+    root=input("Write watching dir way: ") + "\\"
     file_list = []
     for path, dirs, files in os.walk(root):
         for fname in files:
            file_list.append(os.path.join(path, fname))
-
     while file_list:
         file1=file_list.pop(0)
         for file2 in file_list:
@@ -22,3 +19,4 @@ if __name__ == '__main__':
                 print(file1)
                 print(file2)
                 print()
+
