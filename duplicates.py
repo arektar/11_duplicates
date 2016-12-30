9dba500577a6fa5d2971238bebca923bbc89b502
arektar <zander5@mail.ru>
2016-12-30 19:23:52 +0300

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,27 @@
-
+import os, filecmp
 
 def are_files_duplicates(file_path1, file_path_2):
-    pass
+    is_copy=False
+    if os.path.basename(file_path1)==os.path.basename(file_path_2):
+        if os.path.getsize(file_path1)==os.path.getsize(file_path_2):
+            if filecmp.cmp(file_path1,file_path_2):
+                is_copy=True
+    return is_copy
 
 
 if __name__ == '__main__':
-    pass
+    #root=input("Write watching dir way: ")
+    root=r'D:\dropbox'
+    root=root+"\\"
+    file_list = []
+    for path, dirs, files in os.walk(root):
+        for fname in files:
+           file_list.append(os.path.join(path, fname))
+
+    while file_list:
+        file1=file_list.pop(0)
+        for file2 in file_list:
+            if are_files_duplicates(file1,file2):
+                print(file1)
+                print(file2)
+                print()
