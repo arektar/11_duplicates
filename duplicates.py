114f69ebe0a3c9cb827beee5ab5411d2eae358ea
arektar <zander5@mail.ru>
2017-01-17 15:03:05 +0300

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -4,36 +4,43 @@ import itertools
 
 
 def are_files_duplicates(file_path1, file_path_2):
-    if filecmp.cmp(file_path1, file_path_2):
-        return True
-    return False
+    return bool(filecmp.cmp(file_path1, file_path_2))
 
 
-def take_files(root):
-    file_dict = {}
+def receive_all_file_ways(root):
+    root = os.path.abspath(root)
+    dict_of_files = {}
     for path, dirs, files in os.walk(root):
         for f_name in files:
-            if f_name not in file_dict.keys():
-                file_dict[f_name] = list()
-            file_dict[f_name].append(path)
-    return file_dict
+            if f_name not in dict_of_files.keys():
+                dict_of_files[f_name] = list()
+            dict_of_files[f_name].append(path)
+    return dict_of_files
 
 
 def look_files(filedict):
-    equal_list = []
+    dict_of_equal_files = {}
     for name in list(filedict):
         if len(filedict[name]) > 1:
             for way1, way2 in itertools.combinations(filedict[name], 2):
-                if are_files_duplicates(way1 + "\\" + name, way2 + "\\" + name):
-                    equal_list.append((way1 + "\\" + name, way2 + "\\" + name))
-    return equal_list
+                full_filename1 = os.path.join(way1, name)
+                full_filename2 = os.path.join(way2, name)
+                if are_files_duplicates(full_filename1, full_filename2):
+                    if name not in dict_of_equal_files.keys():
+                        dict_of_equal_files[name] = list()
+                    if way1 not in dict_of_equal_files[name]:
+                        dict_of_equal_files[name].append(full_filename1)
+                    if way2 not in dict_of_equal_files[name]:
+                        dict_of_equal_files[name].append(full_filename2)
+    return dict_of_equal_files
 
 
 if __name__ == '__main__':
-    my_root = input("Write watching dir way: ") + "\\"
-    my_file_dict = take_files(my_root)
-    my_equal_list = look_files(my_file_dict)
-    for couple in my_equal_list:
-        print(couple[0])
-        print(couple[1])
+    my_root = input("Write watching dir way: ")
+    my_dict_of_files = receive_all_file_ways(my_root)
+    my_dict_of_equal_files = look_files(my_dict_of_files)
+    print("Duplicates:")
+    for duplicate_file_name in list(my_dict_of_equal_files):
+        for full_duplicate_filename in my_dict_of_equal_files[duplicate_file_name]:
+            print(full_duplicate_filename)
         print()
