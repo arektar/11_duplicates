0c471d86fb711260fc78b6a609bc39298e25a9ad
arektar <zander5@mail.ru>
2017-01-21 18:18:12 +0300

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -2,6 +2,15 @@ import os
 import filecmp
 import itertools
 import collections
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("root_file_path", type=str, )
+    args = parser.parse_args()
+    root_file_path = args.root_file_path
+    return root_file_path
 
 
 def are_files_duplicates(file_path1, file_path_2):
@@ -10,23 +19,21 @@ def are_files_duplicates(file_path1, file_path_2):
 
 def receive_all_file_ways(root):
     root = os.path.abspath(root)
-    dict_of_files = {}
+    dict_of_files = collections.defaultdict(list)
     for path, dirs, files in os.walk(root):
         for f_name in files:
-            if f_name not in dict_of_files.keys():
-                dict_of_files[f_name] = list()
             dict_of_files[f_name].append(path)
     return dict_of_files
 
 
 def look_files(filedict):
     dict_of_equal_files = collections.defaultdict(set)
-    for name in list(filedict):
-        if len(filedict[name]) <= 1:
+    for name, paths in filedict.items():
+        if len(paths) <= 1:
             continue
-        for way1, way2 in itertools.combinations(filedict[name], 2):
-            full_filename1 = os.path.join(way1, name)
-            full_filename2 = os.path.join(way2, name)
+        for first_file_path, second_file_path in itertools.combinations(paths, 2):
+            full_filename1 = os.path.join(first_file_path, name)
+            full_filename2 = os.path.join(second_file_path, name)
             if are_files_duplicates(full_filename1, full_filename2):
                 dict_of_equal_files[name].add(full_filename1)
                 dict_of_equal_files[name].add(full_filename2)
@@ -34,7 +41,7 @@ def look_files(filedict):
 
 
 if __name__ == '__main__':
-    my_root = input("Write watching dir way: ")
+    my_root = parse_arguments()
     my_dict_of_files = receive_all_file_ways(my_root)
     my_dict_of_equal_files = look_files(my_dict_of_files)
     print("Duplicates:")
